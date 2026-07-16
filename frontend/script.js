94a0d151edc2026039871f6160f78212c992d6ff
susmitharedde <susmithardybandi@gmail.com>
2026-07-16 19:54:22 +0530

    Added dashboard navigation pages

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,36 @@
+function registerUser() {
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (name === "" || email === "" || password === "" || confirmPassword === "") {
+        alert("Please fill all fields.");
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match.");
+        return;
+    }
+
+    let user = {
+        name: name,
+        email: email,
+        password: password
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    alert("Registration Successful!");
+
+    window.location.href = "login.html";
+}
+function loginUser(){
+    window.location.href = "result.html";
+}
+
 function submitQuiz() {
 
     let score = 0;
@@ -18,7 +51,47 @@ function submitQuiz() {
         score++;
     }
 
+    // Save score
     localStorage.setItem("score", score);
 
+    // Update completed quiz count
+    let completed = Number(localStorage.getItem("completedQuiz")) || 0;
+    completed++;
+    localStorage.setItem("completedQuiz", completed);
+
+    // Go to result page
     window.location.href = "result.html";
+}
+
+function loadDashboard() {
+
+    let user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+        document.getElementById("username").innerHTML = user.name;
+    }
+
+    let completed = localStorage.getItem("completedQuiz") || 0;
+
+    document.getElementById("progress").innerHTML = completed + " / 10";
+}
+
+function logout() {
+    localStorage.clear();
+    window.location.href = "login.html";
+}
+
+window.onload = function () {
+
+    let score = localStorage.getItem("score");
+
+    if (score == null) {
+        score = 0;
+    }
+
+    let progress = document.getElementById("progress");
+    if (progress) {
+        progress.innerHTML = score + " / 3";
+    }
+
 }
